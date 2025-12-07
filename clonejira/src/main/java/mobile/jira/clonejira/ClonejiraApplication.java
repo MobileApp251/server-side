d9afcb68b746c2e6826b708ab452557a2400e563
dCyberTech <maidangduong92tn@gmail.com>
2025-12-07 20:23:28 +0700

    swagger servers

--- clonejira/src/main/java/mobile/jira/clonejira/ClonejiraApplication.java
+++ clonejira/src/main/java/mobile/jira/clonejira/ClonejiraApplication.java
@@ -1,9 +1,17 @@
 package mobile.jira.clonejira;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+    servers = {
+       @Server(url = "http://localhost:8081", description = "Local Server"),
+       @Server(url = "https://server-side-production-0171.up.railway.app", description = "Production Server")
+    }
+)
 @SpringBootApplication
 @EnableJpaAuditing
 public class ClonejiraApplication {
