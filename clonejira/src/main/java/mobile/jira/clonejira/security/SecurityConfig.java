f81b556ee2425cb7fba1158a359eac8cb63818ee
dCyberTech <maidangduong92tn@gmail.com>
2025-12-16 17:01:18 +0700

    Sercurity

--- clonejira/src/main/java/mobile/jira/clonejira/security/SecurityConfig.java
+++ clonejira/src/main/java/mobile/jira/clonejira/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -18,10 +19,19 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    // @Bean
-    // public AuthenticationManager authenticationManager (AuthenticationConfiguration configuration) throws Exception {
-    //     return configuration.getAuthenticationManager();
-    // }
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(
+            "/swagger-ui/**", 
+            "/v3/api-docs/**", 
+            "/swagger-ui.html"
+        );
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
