ae87af0a3592587d6519ff28208e360ace9ecec8
dCyberTech <maidangduong92tn@gmail.com>
2025-12-28 19:17:40 +0700

    Add new service: [Websocket: Notification]

--- clonejira/src/main/java/mobile/jira/clonejira/config/WebSocketConfig.java
+++ clonejira/src/main/java/mobile/jira/clonejira/config/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package mobile.jira.clonejira.config;
 
+import lombok.RequiredArgsConstructor;
+import mobile.jira.clonejira.security.JwtWebSocketInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,7 +11,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtWebSocketInterceptor interceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -20,5 +26,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(interceptor);
     }
 }
---
+++ clonejira/src/main/java/mobile/jira/clonejira/security/JwtWebSocketInterceptor.java
@@ -0,0 +1,43 @@
+package mobile.jira.clonejira.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtWebSocketInterceptor implements ChannelInterceptor {
+    private final JwtTokenProvider tokenProvider;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            String authHeader = accessor.getFirstNativeHeader("Authorization");
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                String token = authHeader.substring("Bearer ".length());
+
+                if (tokenProvider.validateToken(token)) {
+                    String uid = tokenProvider.getUid(token);
+
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(uid);
+
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                    accessor.setUser(authentication);
+                }
+            }
+        }
+        return message;
+    }
+}
