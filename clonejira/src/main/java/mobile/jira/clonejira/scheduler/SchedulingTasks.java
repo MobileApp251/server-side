99f56eb184aa98496b1a449e361b37fc6ae054c3
dCyberTech <maidangduong92tn@gmail.com>
2026-01-04 18:15:25 +0700

    CI/CD test

--- clonejira/src/main/java/mobile/jira/clonejira/scheduler/SchedulingTasks.java
+++ clonejira/src/main/java/mobile/jira/clonejira/scheduler/SchedulingTasks.java
@@ -10,7 +10,7 @@ public class SchedulingTasks {
     @Scheduled(cron = "0 * * * * *")
     public void checkDueTask() {
         Instant now = Instant.now();
-        System.out.print("checkDueTask >> ");
+        System.out.print("checkDueTask >>> ");
         System.out.println(now);
     }
 }
